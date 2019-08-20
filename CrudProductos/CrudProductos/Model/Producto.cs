5bdf66601f18434a7fa636764acf23be5be1f031
David bolaños <davidebgz@hotmail.com>
2019-08-20 17:44:34 -0600

    cambio1

---
+++ CrudProductos/CrudProductos/Model/Producto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrudProductos.Model
+{
+   public class Producto
+    {
+        public int IdProducto { get; set; }
+    }
+}
