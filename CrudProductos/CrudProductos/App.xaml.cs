34d4e1d0d098a80d3fee1a303d29408d3f5dd9bc
David bolaños <davidebgz@hotmail.com>
2019-08-21 10:38:39 -0600

    nuevo4

--- CrudProductos/CrudProductos/App.xaml.cs
+++ CrudProductos/CrudProductos/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using CrudProductos.View;
 
 namespace CrudProductos
 {
@@ -10,7 +11,7 @@ namespace CrudProductos
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage (new Insertar());
         }
 
         protected override void OnStart()
--- CrudProductos/CrudProductos/View/Insertar.xaml.cs
+++ CrudProductos/CrudProductos/View/Insertar.xaml.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using CrudProductos.Model;
+using System.Collections;
+using System.Collections.ObjectModel;
+
 
 namespace CrudProductos.View
 {
@@ -16,20 +20,46 @@ namespace CrudProductos.View
         {
             InitializeComponent();
         }
-
+        ObservableCollection<Producto> producto_ = new ObservableCollection<Producto>();
         private void Add_Clicked(object sender, EventArgs e)
         {
+            Producto Agregar = new Producto();
+            Agregar.NombreProducto = EntProducto.Text;
+            EntProducto.Text = "";
+            producto_.Add(Agregar);
+            Productos.ItemsSource = producto_;
 
         }
 
         private void BtnVer_Clicked(object sender, EventArgs e)
         {
-
+            Navigation.PushAsync(new Ver());
         }
 
         private void EntGuardar_Clicked(object sender, EventArgs e)
         {
+            Conexion Base = new Conexion();
 
+            Categoria NuevaCategoria = new Categoria();
+            NuevaCategoria.NombreCategoria = EntCategory.Text;
+            NuevaCategoria.Descripcion = EntDescripcion.Text;
+
+            int IdCategoria = Base.InsertarCategoria(NuevaCategoria);
+            if (IdCategoria != -1)
+            {
+                foreach (Producto productoInsertar in producto_)
+                {
+                    productoInsertar.IdCategoria = NuevaCategoria.IdCategoria;
+                    Base.InsertarProducto(productoInsertar);
+
+                }
+                App.Current.MainPage = new NavigationPage(new Ver());
+            }
+            else
+            {
+                DisplayAlert("Alerta   U+26D4", "NO ingreso", "ok");
+            }
+                
         }
     }
 }
